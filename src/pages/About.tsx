21cbea8c5870e8f77f722b6006df690f4aa872b3
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-05 16:34:17 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/11686527-6fca-4a93-a66a-0be6900f229d/src/pages/About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 
 export default function About() {
   useEffect(() => { document.title = 'Vertex IT Solutions - About' }, [])
