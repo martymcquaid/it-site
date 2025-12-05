58afa5bb6c5cb8096d8a5c8815d1bcdc10d152f2
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-05 16:39:29 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/11686527-6fca-4a93-a66a-0be6900f229d/src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import Industries from './pages/Industries'
-import Pricing from './pages/Pricing'
-import Blog from './pages/Blog'
-import BlogPost from './pages/BlogPost'
-import Contact from './pages/Contact'
-import Careers from './pages/Careers'
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/industries" element={<Industries />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/careers" element={<Careers />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
